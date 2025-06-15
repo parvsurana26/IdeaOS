5b06478ecb6de15c654afcd02cd5fea683ec0119
Parv Surana <parvsurana26@gmail.com>
2025-06-16 00:06:02 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // 👈 Add this line to fix asset loading in production
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
